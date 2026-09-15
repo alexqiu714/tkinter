aac2841861c471eb13a3422c2889c231e561a730
alexqiu714 <haode.qiu@isbasel.ch>
2026-09-15 17:26:58 +0200

    Add files via upload

--- homework1.py
+++ homework1.py
@@ -1,22 +1,36 @@
 from tkinter import *
-from time import strftime
 import random
-
-c = ["red","blue","green","purple"]
-
-def t():
-    l.config(text=strftime("%X"))
-    l.after(1000, t)
-    r = random.choice(c)
-    root.config(bg=r)    
+from tkinter import messagebox
 
 root=Tk()
 root.geometry("500x500")
-root.title("Party Clock")
+root.title("math")
 
-l = Label(root, text=strftime("%X"), font=("Arial", 50))
-l.grid(row=0, padx=150,column=0, pady=175)
+def f():
+    global a1
+    r = random.randint(1, 10)
+    r1 = random.randint(1, 10)
+    messagebox.showinfo("quiz", "Calculate the product of " + str(r) + " and " + str(r1))
+    a1=r*r1
 
-t()
+def m():
+    g=int(e.get())
+    if g==a1:
+        messagebox.showinfo("results", "correct! you are a math whiz!")
+    else:
+        messagebox.showinfo("results", "wrong! the answer was " + str(a1))
+    e.delete(0,END)
+
+l=Label(root, text="Quiz", font=("Arial", 20))
+l.grid(row=0,column=0)
+
+b=Button(root, text="quiz", font=("Arial", 20), command=f)
+b.grid(row=1,column=0)
+
+e=Entry(root)
+e.grid(row=2,column=0)
+
+b1=Button(root, text="check", font=("Arial", 20), command=m)
+b1.grid(row=3,column=0)
 
 root.mainloop()
